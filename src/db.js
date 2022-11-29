10a29a27e5416c161b8044fcec7e2a202fd98509
hung <ff789456741@gmail.com>
2022-11-29 18:09:39 +0800

    add firebase config

---
+++ src/db.js
@@ -0,0 +1,14 @@
+import firebase from "firebase/app";
+import "firebase/database";
+require('dotenv').config();
+
+const config = {
+  apiKey: process.env.VUE_APP_FIREBASE_APIKEY,
+  authDomain: process.env.VUE_APP_FIREBASE_AUTHDOMAIN,
+  projectId: process.env.VUE_APP_FIREBASE_PROJECTID,
+  storageBucket: process.env.VUE_APP_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGINGSENDERID,
+  appId: process.env.VUE_APP_FIREBASE_APPID
+};
+const db = firebase.initializeApp(config);
+export default db;
