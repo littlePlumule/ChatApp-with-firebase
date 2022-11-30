b87b37bed73258f487e2d92c36478d82be56543f
hung <ff789456741@gmail.com>
2022-11-30 15:19:53 +0800

    add focus & scrollDown feature

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,19 @@
 <template>
-  <div class="view login" v-if="state.username.trim() === '' || state.username.trim() === null">
+  <div
+    class="view login"
+    v-if="state.username.trim() === '' || state.username.trim() === null"
+  >
     <form class="login-form" @submit.prevent="Login">
       <div class="form-inner">
-        <h1>login to FireChat</h1>
+        <h1>login to ChatApp</h1>
         <label for="username">Username</label>
-        <input 
-          type="text" 
+        <input
+          type="text"
           v-model="inputUsername"
-          placeholder="Please enter your username...">
-        <input type="submit" value="Login">
+          placeholder="Please enter your username..."
+          v-focus
+        />
+        <input type="submit" value="Login" />
       </div>
     </form>
   </div>
@@ -18,10 +23,15 @@
       <h1>Welcome, {{ state.username }}</h1>
     </header>
     <section class="chat-box">
-      <div 
-        v-for="message in state.messages" 
+      <div
+        v-for="message in state.messages"
         :key="message.key"
-        :class="(message.username == state.username ? 'message current-user' : 'message')">
+        :class="
+          message.username == state.username
+            ? 'message current-user'
+            : 'message'
+        "
+      >
         <div class="message-inner">
           <div class="username">{{ message.username }}</div>
           <div class="content">{{ message.content }}</div>
@@ -30,13 +40,13 @@
     </section>
     <footer>
       <form @submit.prevent="SendMessage">
-        <input 
-          type="text" 
+        <input
+          type="text"
           v-model="inputMessage"
-          placeholder="Write a message...">
-        <input 
-          type="submit" 
-          value="Send">
+          placeholder="Write a message..."
+          v-focus
+        />
+        <input type="submit" value="Send" @click="scrollDown" />
       </form>
     </footer>
   </div>
@@ -45,6 +55,12 @@
 <script setup>
 import { reactive, ref, onMounted } from 'vue';
 import db from './db';
+
+const vFocus = {
+  mounted: el => {
+    el.focus();
+  }
+}
 
 const inputUsername = ref("");
 const inputMessage = ref("");
@@ -79,6 +95,11 @@ const SendMessage = () => {
   inputMessage.value = "";
 }
 
+const scrollDown = () => {
+  const body = document.body;
+  body.scrollIntoView({ block: 'end', behavior: 'smooth' });
+}
+
 onMounted(() => {
   const messagesRef = db.database().ref('messages');
 
@@ -102,219 +123,225 @@ onMounted(() => {
 
 <style lang="scss">
 * {
-	font-family: Avenir, Helvetica, Arial, sans-serif;
-	-webkit-font-smoothing: antialiased;
-	-moz-osx-font-smoothing: grayscale;
-	margin: 0;
-	padding: 0;
-	box-sizing: border-box;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
+
+html,
+body {
+  scroll-behavior: smooth;  
+}
+
 .view {
-	display: flex;
-	justify-content: center;
-	min-height: 100vh;
-	background-color: #ea526f;
-	
-	&.login {
-		align-items: center;
-		.login-form {
-			display: block;
-			width: 100%;
-			padding: 15px;
-			
-			.form-inner {
-				display: block;
-				background-color: #FFF;
-				padding: 50px 15px;
-				border-radius: 16px;
-				box-shadow: 0px 6px 12px rgba(0, 0, 0, 0.2);
-				h1 {
-					color: #AAA;
-					font-size: 28px;
-					margin-bottom: 30px;
-				}
-				label {
-					display: block;
-					margin-bottom: 5px;
-					color: #AAA;
-					font-size: 16px;
-					transition: 0.4s;
-				}
-				input[type="text"] {
-					appearance: none;
-					border: none;
-					outline: none;
-					background: none;
-					display: block;
-					width: 100%;
-					padding: 10px 15px;
-					border-radius: 8px;
-					margin-bottom: 15px;
-					
-					color: #333;
-					font-size: 18px;
-					box-shadow: 0px 0px 0px rgba(0, 0, 0, 0);
-					background-color: #F3F3F3;
-					transition: 0.4s;
-					&::placeholder {
-						color: #888;
-						transition: 0.4s;
-					}
-				}
-				input[type="submit"] {
-					appearance: none;
-					border: none;
-					outline: none;
-					background: none;
-					display: block;
-					width: 100%;
-					padding: 10px 15px;
-					background-color: #ea526f;
-					border-radius: 8px;
-					color: #FFF;
-					font-size: 18px;
-					font-weight: 700;
-          cursor: pointer;
-          &:hover {
-            background-color: #e73757;
-          }
-				}
-				&:focus-within {
-					label {
-						color: #ea526f;
-					}
-					input[type="text"] {
-						background-color: #FFF;
-						box-shadow: 0 0 6px rgba(0, 0, 0, 0.2);
-						&::placeholder {
-							color: #666;
-						}
-					}
-				}
-			}
-		}
-	}
-	&.chat {
-		flex-direction: column;
-		header {
-			position: relative;
-			display: block;
-			width: 100%;
-			padding: 50px 30px 10px;
-			.logout {
-				position: absolute;
-				top: 15px;
-				right: 15px;
-				appearance: none;
-				border: none;
-				outline: none;
-				background: none;
-				cursor: pointer;
-				color: #FFF;
-				font-size: 18px;
-				margin-bottom: 10px;
-				text-align: right;
-        &:hover {
-          color: rgba(255, 255, 255, .7);
+  display: flex;
+  justify-content: center;
+  min-height: 100vh;
+  background-color: #ea526f;
+
+  &.login {
+    align-items: center;
+    .login-form {
+      display: block;
+      width: 100%;
+      padding: 15px;
+
+      .form-inner {
+        display: block;
+        background-color: #fff;
+        padding: 50px 15px;
+        border-radius: 16px;
+        box-shadow: 0px 6px 12px rgba(0, 0, 0, 0.2);
+        h1 {
+          color: #aaa;
+          font-size: 28px;
+          margin-bottom: 30px;
         }
-			}
-			h1 {
-				color: #FFF;
-			}
-		}
-		.chat-box {
-			border-radius: 24px 24px 0px 0px;
-			background-color: #FFF;
-			box-shadow: 0px 0px 12px rgba(100, 100, 100, 0.2);
-			flex: 1 1 100%;
-			padding: 30px;
-			.message {
-				display: flex;
-				margin-bottom: 15px;
-				
-				.message-inner {
-					.username {
-						color: #888;
-						font-size: 16px;
-						margin-bottom: 5px;
-						padding-left: 15px;
-						padding-right: 15px;
-					}
-					.content {
-						display: inline-block;
-						padding: 10px 20px;
-						background-color: #F3F3F3;
-						border-radius: 999px;
-						color: #333;
-						font-size: 18px;
-						line-height: 1.2em;
-						text-align: left;
-					}
-				}
-				&.current-user {
-					margin-top: 30px;
-					justify-content: flex-end;
-					text-align: right;
-					.message-inner {
-						max-width: 75%;
-						.content {
-							color: #FFF;
-							font-weight: 600;
-							background-color: #ea526f;
-						}
-					}
-				}
-			}
-		}
-		footer {
-			position: sticky;
-			bottom: 0px;
-			background-color: #FFF;
-			padding: 30px;
-			box-shadow: 0px 0px 12px rgba(100, 100, 100, 0.2);
-			form {
-				display: flex;
-				input[type="text"] {
-					flex: 1 1 100%;
-					appearance: none;
-					border: none;
-					outline: none;
-					background: none;
-					display: block;
-					width: 100%;
-					padding: 10px 15px;
-					border-radius: 8px 0px 0px 8px;
-					
-					color: #333;
-					font-size: 18px;
-					box-shadow: 0px 0px 0px rgba(0, 0, 0, 0);
-					background-color: #F3F3F3;
-					transition: 0.4s;
-					&::placeholder {
-						color: #888;
-						transition: 0.4s;
-					}
-				}
-				
-				input[type="submit"] {
-					appearance: none;
-					border: none;
-					outline: none;
-					background: none;
-					display: block;
-					padding: 10px 15px;
-					border-radius: 0px 8px 8px 0px;
-					background-color: #ea526f;
-					color: #FFF;
-					font-size: 18px;
-					font-weight: 700;
+        label {
+          display: block;
+          margin-bottom: 5px;
+          color: #aaa;
+          font-size: 16px;
+          transition: 0.4s;
+        }
+        input[type="text"] {
+          appearance: none;
+          border: none;
+          outline: none;
+          background: none;
+          display: block;
+          width: 100%;
+          padding: 10px 15px;
+          border-radius: 8px;
+          margin-bottom: 15px;
+
+          color: #333;
+          font-size: 18px;
+          box-shadow: 0px 0px 0px rgba(0, 0, 0, 0);
+          background-color: #f3f3f3;
+          transition: 0.4s;
+          &::placeholder {
+            color: #888;
+            transition: 0.4s;
+          }
+        }
+        input[type="submit"] {
+          appearance: none;
+          border: none;
+          outline: none;
+          background: none;
+          display: block;
+          width: 100%;
+          padding: 10px 15px;
+          background-color: #ea526f;
+          border-radius: 8px;
+          color: #fff;
+          font-size: 18px;
+          font-weight: 700;
           cursor: pointer;
           &:hover {
             background-color: #e73757;
           }
-				}
-			}
-		}
-	}
+        }
+        &:focus-within {
+          label {
+            color: #ea526f;
+          }
+          input[type="text"] {
+            background-color: #fff;
+            box-shadow: 0 0 6px rgba(0, 0, 0, 0.2);
+            &::placeholder {
+              color: #666;
+            }
+          }
+        }
+      }
+    }
+  }
+  &.chat {
+    flex-direction: column;
+    header {
+      position: relative;
+      display: block;
+      width: 100%;
+      padding: 50px 30px 10px;
+      .logout {
+        position: absolute;
+        top: 15px;
+        right: 15px;
+        appearance: none;
+        border: none;
+        outline: none;
+        background: none;
+        cursor: pointer;
+        color: #fff;
+        font-size: 18px;
+        margin-bottom: 10px;
+        text-align: right;
+        &:hover {
+          color: rgba(255, 255, 255, 0.7);
+        }
+      }
+      h1 {
+        color: #fff;
+      }
+    }
+    .chat-box {
+      border-radius: 24px 24px 0px 0px;
+      background-color: #fff;
+      box-shadow: 0px 0px 12px rgba(100, 100, 100, 0.2);
+      flex: 1 1 100%;
+      padding: 30px;
+      .message {
+        display: flex;
+        margin-bottom: 15px;
+
+        .message-inner {
+          .username {
+            color: #888;
+            font-size: 16px;
+            margin-bottom: 5px;
+            padding-left: 15px;
+            padding-right: 15px;
+          }
+          .content {
+            display: inline-block;
+            padding: 10px 20px;
+            background-color: #f3f3f3;
+            border-radius: 999px;
+            color: #333;
+            font-size: 18px;
+            line-height: 1.2em;
+            text-align: left;
+          }
+        }
+        &.current-user {
+          margin-top: 30px;
+          justify-content: flex-end;
+          text-align: right;
+          .message-inner {
+            max-width: 75%;
+            .content {
+              color: #fff;
+              font-weight: 600;
+              background-color: #ea526f;
+            }
+          }
+        }
+      }
+    }
+    footer {
+      position: sticky;
+      bottom: 0px;
+      background-color: #fff;
+      padding: 30px;
+      box-shadow: 0px 0px 12px rgba(100, 100, 100, 0.2);
+      form {
+        display: flex;
+        input[type="text"] {
+          flex: 1 1 100%;
+          appearance: none;
+          border: none;
+          outline: none;
+          background: none;
+          display: block;
+          width: 100%;
+          padding: 10px 15px;
+          border-radius: 8px 0px 0px 8px;
+
+          color: #333;
+          font-size: 18px;
+          box-shadow: 0px 0px 0px rgba(0, 0, 0, 0);
+          background-color: #f3f3f3;
+          transition: 0.4s;
+          &::placeholder {
+            color: #888;
+            transition: 0.4s;
+          }
+        }
+
+        input[type="submit"] {
+          appearance: none;
+          border: none;
+          outline: none;
+          background: none;
+          display: block;
+          padding: 10px 15px;
+          border-radius: 0px 8px 8px 0px;
+          background-color: #ea526f;
+          color: #fff;
+          font-size: 18px;
+          font-weight: 700;
+          cursor: pointer;
+          &:hover {
+            background-color: #e73757;
+          }
+        }
+      }
+    }
+  }
 }
 </style>
